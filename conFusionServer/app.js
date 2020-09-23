a627b21f13a810302697f77b26d0879839e96f2f
shawnsgit <gao0719@gmail.com>
2020-09-23 21:18:04 +1000

    server+database

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+const dishRouter = require('./routes/dishRouter');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
